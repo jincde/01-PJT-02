140ceb23f14016564a3c120c9888e7f2d0cddb58
jincde <asdzxc@kakao.com>
2022-07-22 14:11:45 +0900

    PJT-2, 00.py

---
+++ 2회차/장영진/00.py
@@ -0,0 +1,12 @@
+import requests
+
+order_currency = 'BTC'
+payment_currency = 'KRW'
+URL = f'https://api.bithumb.com/public/ticker/{order_currency}_{payment_currency}'
+
+response = requests.get(URL)
+
+data = response.json()
+result = data.get('data').get('closing_price')
+
+print(result)
